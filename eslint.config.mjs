0cb5a65be6e59ee22b510cc462cd3c1b3dc5a34e
Fabrizio Ossola <ossolafabrizio@gmail.com>
2025-09-30 21:29:15 -0300

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
@@ -20,6 +21,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 })
 
 export const metadata: Metadata = {
-  title: 'Chat Strong Systems',
-  description: 'Chat Strong Systems',
+  title: "Chat Strong Systems",
+  description: "Chat Strong Systems",
 }
 
 export default function RootLayout({
